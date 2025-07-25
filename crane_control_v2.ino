e922b695162a9474df8d879932092a2c0d73ab84
hphilamore <hemma.philamore@bristol.ac.uk>
2025-07-25 18:20:25 +0100

    more buttons added

--- crane_control_v2.ino
+++ crane_control_v2.ino
@@ -15,20 +15,28 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 Servo myservo;  // create Servo object to control a servo
 
 int val;                    // value to send to servo
-int button_pin = 2; 
-int button_status;         // status of push button
-int last_button_status = 1; // previos status of push button
+int button_pin_grab = 2; 
+int button_pin_lift = 3; 
+int button_pin_lower = 4; 
+int button_status_grab;         // status of grab push buttons
+int button_status_lift;
+int button_status_lower;
+int last_button_status_grab = 1; // previous status of grab push button
+
 
 int encoder_count = 0;
 float encoder_distance;
 float last_encoder_distance;
 
 unsigned long t_last_state_change = millis(); // last time the button pin was toggled
-unsigned long debounce_delay;                  // time to wait before confirming button press
+unsigned long debounce_delay = 50;                  // time to wait before confirming button press
 unsigned long t_start, t_end, period; // variables to calculate length of button press
 
 void setup() {
-  pinMode(button_pin, INPUT_PULLUP);  // internal pullup on button pin
+  pinMode(button_pin_grab, INPUT_PULLUP);  // internal pullup on button pin
+  pinMode(button_pin_lift, INPUT_PULLUP);  // internal pullup on button pin
+  pinMode(button_pin_lower, INPUT_PULLUP);  // internal pullup on button pin
+
   myservo.attach(9);                  // attaches the servo on pin 9 to the Servo object
   Serial.begin(115200);
   //Serial.begin(9600);
@@ -48,27 +56,48 @@ void setup() {
 
 void loop() {
   // read the state of the button
-  button_status = digitalRead(button_pin); 
+  button_status_grab = digitalRead(button_pin_grab); 
+  button_status_lift = digitalRead(button_pin_lift); 
+  button_status_lower = digitalRead(button_pin_lower); 
+  
 
-  // check if the button changed state and reset timer
-  if (button_status != last_button_status){
+  // check if the grab button changed state and reset debounce timer
+  if (button_status_grab != last_button_status_grab){
     t_last_state_change = millis();
   }
 
-  // check if the time since the last state change indicates an actual button press
+  // lift crane if button pressed
+  while (button_status_lift == 0){
+    Serial.println("lift");
+    val = 0;
+    myservo.write(val); 
+    button_status_lift = digitalRead(button_pin_lift);
+
+  }
+
+  // lower crane if button pressed
+  while (button_status_lower == 0){
+    Serial.println("lower");
+    val = 180;
+    myservo.write(val); 
+    button_status_lower = digitalRead(button_pin_lower);
+    
+  }
+
+  // check if the time since the last state change indicates an actual grab button press
   if (millis() - t_last_state_change > debounce_delay){
 
     // check button is pressed
-    if (button_status == 0){
+    if (button_status_grab == 0){
 
       t_start = millis(); // time of start of button press
 
       // when the button is pressed and held, lower the crane
-      while (button_status == 0){
+      while (button_status_grab == 0){
         val = 180;
         //Serial.println("lower");
         myservo.write(val); 
-        button_status = digitalRead(button_pin); 
+        button_status_grab = digitalRead(button_pin_grab); 
         encoder("+"); // measure distance lowered using encoder
       }
 
@@ -86,13 +115,16 @@ void loop() {
         encoder("-");
       }
 
+      // tiny delay to compensate for encoder inaccuracy
+      //delay(200);
+
     }
   }
 
   //if the push button is not pressed, stop the servo 
   val = 94;  // stop servo
   myservo.write(val); 
-  last_button_status = button_status; 
+  last_button_status_grab = button_status_grab; 
 
 }  
 
@@ -155,25 +187,25 @@ void encoder(String count_direction){
 
 
 //   // // get the stats of the push button
-//   // button_status = digitalRead(button_pin);            // reads the value of the potentiometer (value between 0 and 1023)
+//   // button_status_grab = digitalRead(button_pin_grab);            // reads the value of the potentiometer (value between 0 and 1023)
    
 //   // if the button is not pressed, do nothing 
-//   while (button_status == 1){
+//   while (button_status_grab == 1){
 //     val = 94;  // stop servo
 //     Serial.println("stop");
 //     myservo.write(val); 
-//     button_status = digitalRead(button_pin);  
+//     button_status_grab = digitalRead(button_pin_grab);  
 
 //   }
 
 //   t_start = millis();
 
 //   // when the button is pressed and held, lower the crane 
-//   while (button_status == 0){
+//   while (button_status_grab == 0){
 //     val = 100;
 //     // Serial.println("lower");
 //     myservo.write(val); 
-//     button_status = digitalRead(button_pin);  
+//     button_status_grab = digitalRead(button_pin_grab);  
 
 //     VL53L0X_RangingMeasurementData_t measure;
     
